ba2a2b825a04aca1bb82391460b46d8779f6674a
helios <lhitaliani@gmail.com>
2016-06-16 05:41:59 -0300

    Cambios en como opera las clases ia-oyente

--- js/ia.js
+++ js/ia.js
@@ -6,15 +6,106 @@ function IA() {
     u.y = this.reubicarCuentas(u.y);
     return u;
   }
+
+  // Ubica la posicion en una posicion Multiplo de 10.
+  // Se ejecutara cuando se ejecute reubicar.
   this.reubicarCuentas = function(numero) {
     var temp = numero;
     if( temp % 10 <= 5 ) temp = temp - (temp % 10);
     else temp = temp + (10 - (temp % 10));
     return temp;
   }
+
   // El parametro debe ser boolean.
   this.opuesto = function(b) {
     return !b;
+  }
+
+  // Si esta en el tablero de dibujo debe analizarse si ya hubo un click.
+  this.permitirDibujo = function(escenario, estado_click, x, y) {
+    var puede;    
+
+    if( escenario.estaEnEscenario(x,y) && !estado_click ) puede = true;
+    else puede = false;
+    return puede;
+  }
+
+  // Verifica que se haya seleccionado un tipo de dibujo.
+  this.teclaCorrecta = function(tecla) {
+    if(tecla == 76 || tecla == 82) return true;
+    else return false;
+  }
+
+  // Cuando se verifico el click y la tecla
+  // Se comienza a construir la Linea o el Rectangulo.
+  this.empezarDibujo = function(coleccion,cvs,clickx,clicky,mousex,mousey,tecla){
+    var c = new Circulo(cvs,3,new Punto(cvs,clickx,clicky));
+    c.color = "red";
+    coleccion.insertar(c);
+
+    if(tecla == 76){
+      var l = new Linea(cvs,new Punto(cvs,clickx,clicky),new Punto(cvs,mousex,mousey));
+      l.color = "blue";
+      coleccion.insertar(l);
+    }
+    else if(tecla == 82){
+      var l = new Rectangulo(cvs,new Punto(cvs,clickx,clicky),mousex,mousey);
+      l.color = "blue";
+      coleccion.insertar(l);
+    }
+    // Requiere despues de esta ejecucion cambiar el estado de activo.
+  }
+
+  this.finalizarDibujo = function(coleccion, coleccion_monitor, cvs, clickx, clicky, tecla) {
+    var l = coleccion.objetos[coleccion.cant - 1];
+    var establecido = false;
+
+    // Establecemos el ultimo punto de la linea.
+    if(tecla == 76) {
+      l.obtenerPF().establecerX(clickx);
+      l.obtenerPF().establecerY(clicky);
+      establecido = true;
+    }
+    // Establecemos las medidas del Rectangulo.
+    else if(tecla == 82) {
+      l.establecerLongitud(clickx - l.obtenerPI().obtenerX());
+      l.establecerAltura(clicky - l.obtenerPI().obtenerY());
+      establecido = true;
+    }
+
+    if(establecido) {
+      var c = new Circulo(cvs,3,new Punto(cvs, clickx, clicky));
+      c.color = "red";
+      coleccion.insertar(c);
+      c = coleccion.objetos[coleccion.cant - 1];
+
+      coleccion_monitor.insertar(enlace_obj.crearEnlace(l));
+      coleccion_monitor.objetos[coleccion_monitor.cant-1].rectangulo.color = coleccion_monitor.objetos[coleccion_monitor.cant-1].color;
+    }
+  }
+
+  this.seleccionEnlace = function(coleccion,x,y) {
+    var i=0;
+    var encontrado=false;
+    var px = x, py = y;
+
+    while(i<coleccion.cant && !encontrado) {
+      var pix = coleccion.objetos[i].rectangulo.obtenerPI().obtenerX();
+      var piy = coleccion.objetos[i].rectangulo.obtenerPI().obtenerY();
+      var pfx = coleccion.objetos[i].rectangulo.obtenerLongitud() + pix;
+      var pfy = coleccion.objetos[i].rectangulo.obtenerAltura() + piy;
+      if (pix<=px && pfx>px && piy<=py && pfy>py) encontrado = true;
+      else i++;
+    }
+    if(!encontrado) i=-1;
+    return i;
+  }
+
+  this.dibujarEnlace = function(coleccion,monitor,i) {
+    coleccion.objetos[i].rectangulo.color = coleccion.objetos[i].colorSeleccion;
+    monitor.pintar();
+    coleccion.dibujarTodo();
+    coleccion.objetos[i].rectangulo.color = coleccion.objetos[i].color;
   }
 
 }
--- js/oyente.js
+++ js/oyente.js
@@ -3,118 +3,72 @@ function Oyente(canvas) {
   Tablero.call(this,canvas);
   this.activo = false;
   this.tecla = null;
+  this.movmousex = 0;
+  this.movmousey = 0;
+  this.clickmousex = 0;
+  this.clickmousey = 0;
+  // Importante para no perder el scope.
+  var self = this;
+
 
   this.escucharTeclado = function() {
     window.addEventListener("keydown",this.apretarTecla,false);
   }
   // Movimiento del mouse.
   this.detectorMovMouse = function() {
-    var canvas = document.getElementById(this.ID);
-    canvas.addEventListener("mousemove", this.movMouse,false);
+    document.getElementById(this.ID).addEventListener("mousemove", this.movMouse,false);
   }
   // Click del mouse.
   this.escucharMouse = function() {
-    var canvas = document.getElementById(this.ID);
-    canvas.addEventListener("mousedown",this.click,false);
+    document.getElementById(this.ID).addEventListener("mousedown",this.click,false);
   }
 
   this.click = function(event) {
     var u = inteligencia.reubicar(event.x,event.y);
-    var cvs = "canvas";
 
-    // Verifica si has hecho un click.
-    if(!o.activo && escenario.estaEnEscenario(u.x,u.y)) {
-      // Dibuja la linea.
-      if(o.tecla == 76) {
-        var c = new Circulo(cvs,3,new Punto(cvs,u.x,u.y));
-        c.color = "red";
-        obj.insertar(c);
-
-        var l = new Linea(cvs,new Punto(cvs,u.x,u.y),new Punto(cvs,this.mousePosX,this.mousePosY));
-        l.color = "blue";
-        obj.insertar(l);
-        o.activo = inteligencia.opuesto(o.activo);
-      }
-      // Dibuja el rectangulo.
-      else if(o.tecla == 82) {
-        var c = new Circulo(cvs,3,new Punto(cvs,u.x,u.y));
-        c.color = "red";
-        obj.insertar(c);
-
-        var l = new Rectangulo(cvs,new Punto(cvs,u.x,u.y),this.mousePosX,this.mousePosY);
-        l.color = "blue";
-        obj.insertar(l);
-        o.activo = inteligencia.opuesto(o.activo);
-      }
+    // Esto se realiza en el area-tablero.
+    // Verifica si el primer click en el tablero tiene algun objeto seleccionado.
+    if(inteligencia.permitirDibujo(escenario,self.activo,u.x,u.y) && inteligencia.teclaCorrecta(self.tecla)) {
+      inteligencia.empezarDibujo(obj,self.ID,u.x,u.y,self.movmousex,self.movmousey,self.tecla);
+      self.activo = inteligencia.opuesto(self.activo);
     }
-    else if(escenario.estaEnEscenario(u.x,u.y)) {
-      var l = obj.objetos[obj.cant - 1];
-      // Creacion Linea.
-      if(o.tecla == 76){
-        l.obtenerPF().establecerX(u.x);
-        l.obtenerPF().establecerY(u.y);
+    // Para terminar necesito que activo sea true y para que sirva en el metodo le doy el valor opuesto.
+    else if(inteligencia.permitirDibujo(escenario,!self.activo,u.x,u.y)){
+      inteligencia.finalizarDibujo(obj,monitor_obj,self.ID,u.x,u.y,self.tecla);
 
-      }
-      // Creacion Rectangulo.
-      else {
-        l.establecerLongitud(u.x - l.obtenerPI().obtenerX());
-        l.establecerAltura(u.y - l.obtenerPI().obtenerY());
-      }
-      // Creacion del acceso directo.
-      monitor_obj.insertar(enlace_obj.crearEnlace(l));
-      monitor_obj.objetos[monitor_obj.cant-1].rectangulo.color = monitor_obj.objetos[monitor_obj.cant-1].color;
-
-      var c = new Circulo(cvs,3,new Punto(cvs,u.x,u.y));
-      c.color = "red";
-      obj.insertar(c);
-      c = obj.objetos[obj.cant - 1];
-
-      tablero.limpiar();
+      self.limpiar();
       monitor_obj.dibujarTodo();
       obj.dibujarTodo();
-      o.activo = inteligencia.opuesto(o.activo);
+      self.activo = inteligencia.opuesto(self.activo);
     }
-    // Cuando se hace click en el monitor_objetos.
-    else if(monitor_objetos.estaEnEscenario(event.x,event.y)) {
-      var i=0;
-      var encontrado=false;
-      var px = event.x; var py = event.y;
 
-      while(i<monitor_obj.cant && !encontrado) {
-        var pix = monitor_obj.objetos[i].rectangulo.obtenerPI().obtenerX();
-        var piy = monitor_obj.objetos[i].rectangulo.obtenerPI().obtenerY();
-        var pfx = monitor_obj.objetos[i].rectangulo.obtenerLongitud() + pix;
-        var pfy = monitor_obj.objetos[i].rectangulo.obtenerAltura() + piy;
-        if (pix<=px && pfx>px && piy<=py && pfy>py) encontrado = true;
-        else i++;
-      }
-
-      if(encontrado) {
-        monitor_obj.objetos[i].rectangulo.color = monitor_obj.objetos[i].colorSeleccion;
-        monitor_objetos.pintar();
-        monitor_obj.dibujarTodo();
-        monitor_obj.objetos[i].rectangulo.color = monitor_obj.objetos[i].color;
-      }
+    // Esto se realiza en el area-monitor.
+    else if(inteligencia.permitirDibujo(monitor_objetos,self.activo,u.x,u.y)) {
+      var i = inteligencia.seleccionEnlace(monitor_obj,event.x,event.y);
+      if(i != -1){
+        inteligencia.dibujarEnlace(monitor_obj,monitor_objetos,i);
+      };
     }
 
   }
+
   this.movMouse = function(event) {
-    var mousePosX = event.clientX;
-    var mousePosY = event.clientY;
+    self.movmousex = event.clientX;
+    self.movmousey = event.clientY;
 
     // Movimiento de la linea.
-    if(o.activo) {
+    if(self.activo) {
       var l = obj.objetos[obj.cant - 1];
-      if(o.tecla == 76) {
-        l.obtenerPF().establecerX(mousePosX);
-        l.obtenerPF().establecerY(mousePosY);
+      if(self.tecla == 76) {
+        l.obtenerPF().establecerX(self.movmousex);
+        l.obtenerPF().establecerY(self.movmousey);
       }
       else {
-        l.establecerLongitud(mousePosX - l.obtenerPI().obtenerX());
-        l.establecerAltura(mousePosY - l.obtenerPI().obtenerY());
+        l.establecerLongitud(self.movmousex - l.obtenerPI().obtenerX());
+        l.establecerAltura(self.movmousey - l.obtenerPI().obtenerY());
       }
 
-      var p = inteligencia.reubicar(mousePosX,mousePosY);
+      var p = inteligencia.reubicar(self.movmousex,self.movmousey);
       tablero.limpiar();
       if( escenario.estaEnEscenario(p.x,p.y)) obj.dibujarOpt(p.x/10 - 20,p.y/10 - 20,p.x/10 + 20,p.y/10 + 20);
       else {
@@ -123,26 +77,26 @@ function Oyente(canvas) {
       }
     }
   }
-  // Este metodo todavia no esta implementado.
+
   this.apretarTecla = function(event) {
-    console.log(event.keyCode);
+    // console.log(event.keyCode);
     // tecla: l
     if(event.keyCode==76) {
-      o.tecla = 76;
+      self.tecla = 76;
     }
     // tecla: r
     else if(event.keyCode==82) {
-      o.tecla = 82;
+      self.tecla = 82;
     }
     // tecla: Esc
     else if(event.keyCode==27) {
-      o.tecla = 27;
+      self.tecla = 27;
 
-      if(o.activo) {
+      if(self.activo) {
         // Elimina el ultimo.
         for(var i=0; i<2;i++) obj.eliminar(obj.objetos[obj.cant - 1]);
 
-        o.activo = inteligencia.opuesto(o.activo);
+        self.activo = inteligencia.opuesto(self.activo);
         tablero.limpiar();
         monitor_obj.dibujarTodo();
         obj.dibujarTodo();
